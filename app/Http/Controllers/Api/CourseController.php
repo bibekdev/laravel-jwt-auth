6afe0fefd867222c8c4fc87610a0569da0e72d07
Bibek Chaudhary <kingbibekc@gmail.com>
2022-03-18 23:01:58 +0545

    updated user and course controller and added routes of api

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function courseEnrollment(Request $request)
+    {
+    }
+
+    public function totalCourse()
+    {
+    }
+
+    public function deleteCourse($id)
+    {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::group(['middleware' => ['api']], function () {
+    Route::get('profile', [UserController::class, 'profile']);
+    Route::get('logout', [UserController::class, 'logout']);
+
+    // course api roues
+    Route::post('create-course', [CourseController::class, 'courseEnrollment']);
+    Route::get('total-course', [CourseController::class, 'totalCourse']);
+    Route::delete('delete-course/{id}', [CourseController::class, 'deleteCourse']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
